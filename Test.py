a5d4889a65dd7100224b3bfb3df21b56df934e98
TiaVerwega <98816775+TiaVerwega@users.noreply.github.com>
2022-02-01 12:51:02 +0100

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,5 @@
+import numpy as np
+import package_pkg as package
+
+print(package.function(5))
+
---
+++ package_pkg/package.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def function(data):
+    n = 5
+    
+    return data * n
+    
