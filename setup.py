2a808188d0d3d4124231041f79c163169b108e0b
TiaVerwega <98816775+TiaVerwega@users.noreply.github.com>
2022-02-02 09:49:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-Maria-TheresiaVerwega", # Replace with your own username
+    name="TestPackage", # Replace with your own username
     version="0.0.1",
     author="Maria-Theresia Verwega",
     author_email="mtv@informatik.uni-kiel.de",
